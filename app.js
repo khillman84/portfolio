4c85a924d04e4f2652a5e9a1c7d6601a377c2231
Kyle Hillman <khillman84@gmail.com>
2017-02-08 11:46:30 -0800

    added mobile first styling and additional css styling

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict'
+
+function Projects(name, description, date, link, github) {
+  this.name = name;
+  this.description = description;
+  this.date = date;
+  this.link = link;
+  this.github = github;
+}
+
+console.log(Projects);
