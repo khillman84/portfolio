841c79eb56d89e6ecefd3a96d4e173c0102d0f1c
Kyle Hillman <khillman84@gmail.com>
2017-02-10 08:25:17 -0800

    added social media links and added code to projects.js to allow for site content to be dynamically generated onto the website

---
+++ scripts/projectData.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var projectData = [
+  {
+    name:         'Project 1',
+    image:        'img/blank.png',
+    description:  'This is a descripton of the project',
+    date:         '1 Jan 17',
+    link:         'project link',
+  },
+  {
+    name:         'Project 2',
+    image:        'img/blank.png',
+    description:  'This is a descripton of the project',
+    date:         '2 Feb 17',
+    link:         'project link',
+  },
+  {
+    name:         'Project 3',
+    image:        'img/blank.png',
+    description:  'This is a descripton of the project',
+    date:         '3 Mar 17',
+    link:         'project link',
+  },
+];
---
+++ scripts/projects.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var allProjects = [];
+
+function Projects(cat) {
+  this.name = cat.name;
+  this.image = cat.image;
+  this.description = cat.description;
+  this.date = cat.date;
+  this.link = cat.link;
+}
+
+Projects.prototype.toHtml = function() {
+  var $newProject = $('summary.template').clone().removeClass('template');
+
+  $newProject.find('address').text('this.name');
+  $newProject.find('img').attr('href', this.image);
+  $newProject.find('.project-description').text('cat.description');
+  $newProject.find('time').text('cat.date');
+  $newProject.find('.projects a').attr('href', this.link);
+  return $newProject;
+};
+
+projectData.forEach(function(ele){
+  allProjects.push(new Projects(ele));
+});
+
+allProjects.forEach(function(a){
+  $('#projects').append(a.toHtml());
+});
