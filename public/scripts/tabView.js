a2d26641effac9111866ac4a7d1974d271287d41
Kyle Hillman <khillman84@gmail.com>
2017-02-20 08:18:31 -0800

    added more functional programming and wrapped my JS in closures

--- public/scripts/tabView.js
+++ public/scripts/tabView.js
@@ -1,18 +1,23 @@
-'use strict'
+(function(module){
+  'use strict'
+  const tabView = {};
 
-const tabView = {};
+  //Show or hide content based on what nav bar item is clicked
+  tabView.handleNav = () => {
+    $('.navigation').on('click', '.tab', function() {
+      $('.tab-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    });
+    $('.navigation .tab:first').click();
+  };
 
-tabView.handleNav = function() {
-  $('.navigation').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
-  });
-  $('.navigation .tab:first').click();
-};
+  //Initialize the index page and attach the project data to the webpage
+  tabView.initIndexPage = () => {
+    Projects.all.map(function(a){
+      $('#projects').append(a.toHtml())
+    });
+    tabView.handleNav();
+  };
 
-tabView.initIndexPage = function() {
-  Projects.all.forEach(function(a){
-    $('#projects').append(a.toHtml())
-  });
-  tabView.handleNav();
-};
+  module.tabView = tabView;
+})(window);
